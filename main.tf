190c19ed19de208a8639d3043cc62d1d135fb4cc
r0naldy <ronaldyitolopez@gmail.com>
2025-06-18 22:13:58 -0500

    Agrego archivos iniciales de configuración Terraform esqueleto

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "nombre-lambda" # update
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "nombre_funcion-lambda" # update
+  description   = "Mi función Lambda de ejemplo"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.13"
+  filename      = ""
+  source_code_hash = filebase64sha256("")
+ 
+  environment {
+  
+    variables = {
+      ENV = "dev"
+    }
+  }
+}
