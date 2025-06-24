be410ae09f7adee96433a74810dd787007239de9
jean_franco <jeanfrancolaura4@gmail.com>
2025-06-24 05:55:15 -0500

    Empaquetado automático de Lambda desde raíz del proyecto

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 provider "aws" {
-  region     = var.aws_region
+  region = var.aws_region
 }
 
+# 🧩 Empaquetar Lambda desde la raíz del submódulo
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/.."               # carpeta raíz del proyecto_lambda
+  output_path = "${path.module}/../function.zip"
+}
+
+# Rol de ejecución para Lambda
 resource "aws_iam_role" "lambda_exec_role" {
-  name = "nombre-lambda" # update
+  name = "lambda_exec_role_jean_franco"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -13,27 +21,29 @@ resource "aws_iam_role" "lambda_exec_role" {
         Service = "lambda.amazonaws.com"
       },
       Effect = "Allow",
-      Sid = ""
+      Sid    = ""
     }]
   })
 }
 
+# Adjuntar permisos de logs
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role = aws_iam_role.lambda_exec_role.name
+  role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Función Lambda principal
 resource "aws_lambda_function" "my_lambda" {
-  function_name = "nombre_funcion-lambda" # update
-  description   = "Mi función Lambda de ejemplo"
+  function_name = "proyecto_lambda_reportes"
+  description   = "Función Lambda para procesar CSV desde S3"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "lambda_function.lambda_handler"
+  handler       = "main.lambda_handler"               # Asegúrate que coincida con tu archivo
   runtime       = "python3.13"
-  filename      = ""
-  source_code_hash = filebase64sha256("")
- 
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
   environment {
-  
     variables = {
       ENV = "dev"
     }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,2 @@
-aws_access_key_id     = ""
-aws_secret_access_key = ""
 aws_region            = "us-east-1"
 bucket_name           = "proyecto-cloud-bucket-unico-2025"
